81117394bfe0a2e0761c5aa870c483e651b224c7
Kantharaju <kantharajucn@outlook.com>
2018-05-26 02:01:08 +0200

    passed the file as arguement and creating the directory if it does not exixt

--- src/downloadImage.py
+++ src/downloadImage.py
@@ -1,25 +1,39 @@
 import urllib
 import os
+import argparse
 
 
 class DownloadImage:
 
-    def download_images(self, file_name):
-        try:
-            with open(file_name) as f:
-                for line in f:
-                    image = line.rpartition('/')[2]
-                    image_path = os.path.join('../output', image)
-                    urllib.urlretrieve(line, image_path)
-        except IOError:
-            print("Could not read file {}".format(file_name))
+    def __init__(self, url_file):
+        self.url_file = url_file
+
+    def download_images(self):
+
+        """Downloading the images from the urls provided in the file to the output directory"""
+
+        # Checking if the output directory exists, If not exists create the directory output
+        if not os.path.exists("../output"):
+            os.mkdir("../output")
+
+        with open(self.url_file) as f:
+            for line in f:
+                image = line.rpartition('/')[2]
+                image = image.strip()
+                image_path = os.path.join('../output', image)
+                urllib.urlretrieve(line, image_path)
+        print("Downloading the images completed")
 
 
-def main():
-    download = DownloadImage()
-    file_name = os.path.join('../urls/', 'urls.txt')
-    download.download_images(file_name)
+def main(url_file):
+    download = DownloadImage(url_file)
+    download.download_images()
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Downloading Images from web')
+    parser.add_argument('--file', required=True, help='../file.txt')
+    parser.print_help()
+    args = parser.parse_args()
+    print(args.file)
+    main(args.file)
