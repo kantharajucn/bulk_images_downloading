2b61ff30fdd23a263f3e6d32251585f786a75d79
Kantharaju <kantharajucn@outlook.com>
2018-05-26 12:51:04 +0200

    added some more unit tests

--- src/unitTests.py
+++ src/unitTests.py
@@ -1,25 +1,32 @@
 import os
 import unittest
+from downloadImages import DownloadImage
 
 
+class TestDownloadImage(unittest.TestCase):
 
-class TestDOwnloadImage(unittest.TestCase):
+    def setUp(self):
+        print('TestDownloadImage.__init__')
+        self.url_file = "../urls/urls.txt"
+        self.output = "../output"
+        self.download = DownloadImage(self.url_file)
 
     def test_check_file(self):
-        self.assertTrue(os.stat('../urls/urls.txt').st_size > 0)
+        self.assertTrue(os.stat(self.url_file).st_size > 0)
 
     def test_check_urls(self):
-        with open('../urls/urls.txt') as f:
+        with open(self.url_file) as f:
             for line in f:
                 self.assertTrue(line.startswith('http'))
 
-    def test_check_download(self):
-        num_urls = 0
-        num_images = 0
-        with open('../urls/urls.txt') as f:
+    def test_download(self):
+        self.assertTrue(len(os.listdir("../urls")),0)
+
+    def test_verify_output(self):
+        with open(self.url_file) as f:
             num_urls = sum(1 for _ in f)
-        num_images = len([name for name in os.listdir('../output')])
-        self.assertTrue(num_urls  == num_images)
+        num_images = len([name for name in os.listdir(self.output)])
+        self.assertTrue(num_urls == num_images)
 
 
 if __name__ == '__main__':
